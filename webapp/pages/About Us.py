c15263b94b43a977c09e32d3c9f8c98c778f8966
MMysore2 <61029417+MMysore2@users.noreply.github.com>
2024-05-07 17:17:29 -0700

    Update About Us.py

--- webapp/pages/About Us.py
+++ webapp/pages/About Us.py
@@ -27,7 +27,8 @@ def display_members():
     jacob_pfp = MysoreLeft.image("../webapp/static/img/waldo_icon.png")
 
     jacobRight.markdown("<center><h1> Jacob Marinas </h1></center>", unsafe_allow_html=True)
-    jacobRight.write("\nNice biograpghy")
+    jacobRight.write("\nhi, I’m Jacob - 3rd year CS / Math @ UC Davis : ) a few facts about me:\ncame up here from Socal - I like experiencing and learning new things every day
+previously did FTC / FRC robotics, game dev\nenjoy working on interesting / challenging problems with others - that’s why I’m here in GDSC : )\nalso a PM in CodeLab, working with the UC Davis Center for Educational Effectiveness (also CS Peer Advisor in training :o)\nother interests: cubing, rhythm games, dance, gym, tech, productivity\nfun fact: I’ve solved 9 Rubik’s Cubes in a row, blindfolded")
 
     mysoreLeft, mysoreRight  = st.columns(2)
 
@@ -35,7 +36,7 @@ def display_members():
     mysore_pfp = mysoreLeft.image("../webapp/static/img/waldo_icon.png")
 
     mysoreRight.markdown("<center><h1> Mitha Mysore </h1></center>", unsafe_allow_html=True)
-    mysoreRight.write("\nNice biograpghy about Mysore ")
+    mysoreRight.write("\nHi! I’m Mysore! I’m a 3rd year CS major at UC Davis.\nWhen I’m not coding, you can find me drawing and crocheting! I also have two pet parakeets who love playing Minecraft with me.\nI took on this project to expand my toolkit, but it also taught me how to start a project from scratch. I hope to use what I learned to create more projects in the future!")
 
 
 
